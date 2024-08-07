eab532d6b0bf44f514548aeb54a502afee7d08fe
Firman-001 <pirman475@smk.belajar.id>
2024-08-07 14:57:49 +0700

    Soal keenam

---
+++ nomor_6.1.js
@@ -0,0 +1,12 @@
+const tambah = (x, y) => {
+    return x + y;
+};
+
+const kali = (x, y) => {
+    return x * y;
+};
+
+module.exports = {
+    tambah, 
+    kali
+};
---
+++ nomor_6.2.js
@@ -0,0 +1,6 @@
+const { tambah, kali } = require('./nomor_6.1')
+const sum = tambah(26, 2);
+console.log(`Hasil Penjumlahan: ${sum}`);
+
+const multiply = kali(5, 2);
+console.log(`Hasil Perkalian: ${multiply}`);
