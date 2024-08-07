7291dcff6ccd6a267370a4b1a8308626e770c977
Firman-001 <pirman475@smk.belajar.id>
2024-08-07 14:46:37 +0700

    Soal keempat

---
+++ nomor_4.js
@@ -0,0 +1,7 @@
+const [kota = "Bekasi", kabupaten = "Setu"] = ["red"];
+console.log(kota);
+console.log(kabupaten);
+
+const { daerah = "Jakarta", negara = "Indonesia" } = { name: "Mufiz", negara: "Italia" };
+console.log(daerah);
+console.log(negara);
